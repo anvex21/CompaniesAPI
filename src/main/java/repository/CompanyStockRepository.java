e849e2841f9f25d01850ec896b67050fce463d4d
anvex21 <alkinvejdi@gmail.com>
2025-08-29 00:13:54 +0300

    Add @ApplicationScoped to the repo

--- src/main/java/repository/CompanyStockRepository.java
+++ src/main/java/repository/CompanyStockRepository.java
@@ -2,9 +2,11 @@ package repository;
 
 import entity.CompanyStock;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
 
+@ApplicationScoped
 public class CompanyStockRepository implements PanacheRepository<CompanyStock> {
     public CompanyStock findLatestByCompanyId(Long companyId) {
         // Returns the latest CompanyStock entry for the given companyId
